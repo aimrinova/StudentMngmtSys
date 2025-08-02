d498abea3829699997442acde944acceffb13ff8
Daniel Schubert <daschub496@gmail.com>
2025-08-02 16:11:25 +0200

    init commit

---
+++ src/main/java/org/anubis/lectures/studentmngmtsys/HelloApplication.java
@@ -0,0 +1,60 @@
+package org.anubis.lectures.studentmngmtsys;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void init() {
+        System.out.println("Before");
+    }
+    // chapter1/StageStylesDemo
+    Stage stage = new Stage(StageStyle.UNDECORATED);
+    // or
+    // stage.initStyle(StageStyle.TRANSPARENT)
+
+    @Override
+    public void start(Stage stage1) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage1.setTitle("Hello!");
+        stage1.setScene(scene);
+        stage1.show();
+
+        // this window doesn't block mouse and keyboard events
+        Stage stage2 = new Stage();
+        stage2.setTitle("I don't block anything");
+        stage2.initModality(Modality.NONE);
+        stage2.show();
+        // this window blocks everything - you can't interact
+        // with other windows while it's open
+        Stage stage3 = new Stage();
+        stage3.setTitle("I block everything");
+        stage3.initModality(Modality.APPLICATION_MODAL);
+        stage3.show();
+        // this window blocks only interaction with it's owner window(stage1)
+        Stage stage4 = new Stage();
+        stage4.setTitle("I block only clicks to main window");
+        stage4.initOwner(stage1);
+        stage4.initModality(Modality.WINDOW_MODAL);
+        stage4.show();
+
+        Stage stage = new Stage(StageStyle.UNDECORATED);
+        stage.show();
+    }
+
+    public void stop() {
+        System.out.println("After");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/org/anubis/lectures/studentmngmtsys/HelloController.java
@@ -0,0 +1,14 @@
+package org.anubis.lectures.studentmngmtsys;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
